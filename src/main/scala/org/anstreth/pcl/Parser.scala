d95ac1ed2c13aaaa7a2c46e7d8d2f8e5dc7c154c
Roman Golyshev <anstreth@gmail.com>
2017-09-16 14:48:11 +0300

    remove comments

--- src/main/scala/org/anstreth/pcl/Parser.scala
+++ src/main/scala/org/anstreth/pcl/Parser.scala
@@ -1,16 +1,5 @@
 package org.anstreth.pcl
 
-/**
-  * Generic string parser trait.
-  *
-  * @tparam T the type of the parsing.
-  */
 trait Parser[T] {
-  /**
-    * Method to parse string and into something and return the result
-    *
-    * @param s String to parse
-    * @return (String, T) tuple, in which T is a parse result, and String is remained string
-    */
   def parse(s: String): (String, T)
 }
