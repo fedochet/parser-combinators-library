f2ab9e38de3e9b87bf0ea2a2b195a76a63a354b7
Roman Golyshev <anstreth@gmail.com>
2017-09-17 17:18:14 +0300

    move map function to Parser trait

--- src/main/scala/org/anstreth/pcl/Parser.scala
+++ src/main/scala/org/anstreth/pcl/Parser.scala
@@ -2,4 +2,11 @@ package org.anstreth.pcl
 
 trait Parser[T] {
   def parse(s: String): (String, ParseResult[T])
+  def map[P](map: T => P): Parser[P] = (original) => {
+    parse(original) match {
+      case (remainder, Success(t)) => (remainder, Success(map(t)))
+      case (remainder, Error()) => (remainder, Error());
+    }
+  }
+
 }
